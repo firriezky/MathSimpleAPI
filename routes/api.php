cef2957ffd158b733cdc4dd0c4a3aa927aab64a4
firriezky <firriezky@gmail.com>
2021-03-19 22:08:50 +0700

    add slash at api route

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::any('formula/fetch','FormulaController@fetchAll');
-Route::any('questionex/fetch','QuestionexController@fetchAll');
-Route::any('notation/fetch','NotasiController@fetchAll');
+Route::any('/formula/fetch','FormulaController@fetchAll');
+Route::any('/questionex/fetch','QuestionexController@fetchAll');
+Route::any('/notation/fetch','NotasiController@fetchAll');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
